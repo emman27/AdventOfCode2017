510983a32ee401c395f55514ea4a2bf0bfafdb7d
Emmanuel Goh <emmanuel@cynopsis-solutions.com>
2018-01-11 14:36:25 +0800

    AOC 2

--- inverseCaptcha/inverseCaptcha.go
+++ inverseCaptcha/inverseCaptcha.go
@@ -24,3 +24,23 @@ func InverseCaptcha(s string) int {
 	}
 	return total
 }
+
+// InverseCaptchaVersion2 also considers the halfway round character
+func InverseCaptchaVersion2(s string) int {
+	total := 0
+	half := len(s) / 2
+	for i := 0; i < len(s); i++ {
+		wanted := i + half
+		if i+half >= len(s) {
+			wanted -= len(s)
+		}
+		if s[i] == s[wanted] {
+			val, err := strconv.Atoi(string(s[i]))
+			if err != nil {
+				panic(err)
+			}
+			total += val
+		}
+	}
+	return total
+}
--- inverseCaptcha/inverseCaptcha_test.go
+++ inverseCaptcha/inverseCaptcha_test.go
@@ -21,3 +21,19 @@ func TestInverseCaptcha(t *testing.T) {
 		}
 	}
 }
+
+func TestInverseCaptchaVersion2(t *testing.T) {
+	cases := []testCase{
+		{Input: "1212", Output: 6},
+		{Input: "1221", Output: 0},
+		{Input: "123425", Output: 4},
+		{Input: "123123", Output: 12},
+		{Input: "12131415", Output: 4},
+	}
+	for _, c := range cases {
+		actual := InverseCaptchaVersion2(c.Input)
+		if actual != c.Output {
+			t.Fatalf("Test failed, Actual: %d, Expected %d", actual, c.Output)
+		}
+	}
+}
