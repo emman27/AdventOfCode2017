15c67fceeb997f69475a7d2241b10ff2a26c21a1
Emmanuel Goh <emmanuel@cynopsis-solutions.com>
2018-01-11 16:26:44 +0800

    AOC 2b

--- corruptionChecksum/functions.go
+++ corruptionChecksum/functions.go
@@ -1,16 +1,19 @@
 package corruptionChecksum
 
+import "errors"
+
 // Calculate generates the checksum by finding the maximum difference between the smallest and largest values for each row, then summing these differences
-func Calculate(matrix [][]int, calculatorFunction func([]int) int) int {
+func Calculate(matrix [][]int, calculatorFunction func([]int) (int, error)) int {
 	var total int
 	for _, row := range matrix {
-		total += calculatorFunction(row)
+		val, _ := calculatorFunction(row)
+		total += val
 	}
 	return total
 }
 
 // MaxDiff calculates the maximum diff in a row
-func MaxDiff(row []int) int {
+func MaxDiff(row []int) (int, error) {
 	max := -2 ^ 21
 	min := 2 ^ 21
 	for _, num := range row {
@@ -21,10 +24,19 @@ func MaxDiff(row []int) int {
 			min = num
 		}
 	}
-	return max - min
+	return max - min, nil
 }
 
 // EvenDivisResults finds the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
-func EvenDivisResults(row []int) int {
-	return 0
+func EvenDivisResults(row []int) (int, error) {
+	for idx, num := range row {
+		for i := idx + 1; i < len(row); i++ {
+			if num%row[i] == 0 {
+				return num / row[i], nil
+			} else if row[i]%num == 0 {
+				return row[i] / num, nil
+			}
+		}
+	}
+	return 0, errors.New("Cannot find a divisible pair")
 }
--- corruptionChecksum/functions_test.go
+++ corruptionChecksum/functions_test.go
@@ -17,3 +17,21 @@ func TestCalcuateMaxDiff(t *testing.T) {
 		}
 	}
 }
+
+type t2Case struct {
+	Input    []int
+	Expected int
+}
+
+func TestEvenDivisResults(t *testing.T) {
+	cases := []t2Case{
+		{Input: []int{5, 9, 2, 8}, Expected: 4},
+		{Input: []int{9, 4, 7, 3}, Expected: 3},
+		{Input: []int{3, 8, 6, 5}, Expected: 2},
+	}
+	for idx, c := range cases {
+		if res, err := EvenDivisResults(c.Input); res != c.Expected || err != nil {
+			t.Fatalf("Test #%d failed. Expected: %d, Actual: %d", idx, c.Expected, res)
+		}
+	}
+}
